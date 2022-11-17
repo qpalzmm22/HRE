6789a553fe241d9e8c9db71cffb87b9d85720e3d
qpalzmm22 <qpalzmm22@gmail.com>
2022-11-17 14:18:04 +0900

    Configure authentication, firebase and create some pages

---
+++ lib/home.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+
+class HomePage extends StatefulWidget {
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Main"),
+        ),
+        body: Column(
+            children: const [
+              Text("Do this"),
+            ]
+        )
+    );
+  }
+}
